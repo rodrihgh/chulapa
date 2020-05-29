5f3621de088b9d36567ec6d3b580bb596605e3f8
dieghernan <diego.hernangomez@gmail.com>
2020-05-29 11:35:27 +0000

    Fix algolia

--- assets/js/algolia/algolia-search.js
+++ assets/js/algolia/algolia-search.js
@@ -2,8 +2,6 @@
 layout: null
 ---
 
-var baseurl = "{{ site.baseurl }}";
-
 const algoliaClient = algoliasearch(
   'KOIKA5PDQO',
   '7f1fe42bef3f03458cb3a911ebc7e0ac'
@@ -29,7 +27,6 @@ const search = instantsearch({
   indexName: 'chulapa',
   searchClient,
 });
-{% raw %}
 search.addWidgets([
   instantsearch.widgets.searchBox({
     container: '#searchbox',
@@ -55,7 +52,7 @@ search.addWidgets([
       `,
       item: `
         <article>
-          <h4 class="chulapa-links-hover-only"><a href="baseurl{{url}}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a></h4>
+          <h4 class="chulapa-links-hover-only"><a href="{{ {{url}} | absolute_url }}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a></h4>
           <h6>{{subtitle}}</h5>
           <p>{{#helpers.highlight}}{ "attribute": "content" }{{/helpers.highlight}}<a href="baseurl{{url}}"> [+]</a></p>
         </article>
